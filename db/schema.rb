db1833a80fe45921e63314cfc62ee6afd3ff2c2b
Chris Castle <castle@heroku.com>
2018-03-12 10:53:08 -0700

    Add index to user name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311234228) do
+ActiveRecord::Schema.define(version: 20180312033940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180311234228) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name"
   end
 
   add_foreign_key "badges", "units"
