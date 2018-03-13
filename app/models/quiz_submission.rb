246818dc47b5d276350e4a3a02069a9f319b79ea
Chris Castle <castle@heroku.com>
2018-03-12 22:00:58 -0700

    Fix bug where unanswered question counted as correct

--- app/models/quiz_submission.rb
+++ app/models/quiz_submission.rb
@@ -2,7 +2,11 @@ class QuizSubmission < ApplicationRecord
   belongs_to :user
   belongs_to :unit
 
+  # Ensure both all answers are correct and all questions are answered
+  #  i.e. a blank answer is incorrect
   def correct?
+    return false unless answers.count == unit.questions.count
+
     answers.each do |question_id, answer_index|
       return false unless answer_is_correct? question_id, answer_index
     end
