23e3314fce7a56e448cfe27d5dc39a6d87cf55f3
Chris Castle <castle@heroku.com>
2018-03-20 11:17:44 -0700

    Fix exception thrown when user asks for invalid user id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   helper_method :logged_in?
