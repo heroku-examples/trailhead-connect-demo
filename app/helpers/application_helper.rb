580949207e0db85f5f0b04be0976ac5a263ebb46
Chris Castle <castle@heroku.com>
2018-03-21 15:24:22 -0700

    Dynamically apply active class to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  ACTIVE_CLASS = 'active'.freeze
+
+  def active_for(options)
+    name_of_controller = options.fetch(:controller) { nil }
+    name_of_action     = options.fetch(:action) { nil }
+    request_path       = options.fetch(:path) { nil }
+  
+    return ACTIVE_CLASS if request_path && request_path == request.path
+  
+    if name_of_controller == controller_name
+      ACTIVE_CLASS if name_of_action.nil? || (name_of_action == action_name)
+    end
+  end
 end
